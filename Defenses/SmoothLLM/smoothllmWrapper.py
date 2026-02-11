94bf4ba348ed7311001aa4315c4b30d49988d5ed
Janelys G <janelysgracianobetancourt2026@u.northwestern.edu>
2026-02-11 00:36:45 -0600

    edited wrapperSmoothllm

--- Defenses/SmoothLLM/smoothllmWrapper.py
+++ Defenses/SmoothLLM/smoothllmWrapper.py
@@ -1,22 +1,18 @@
+#importing official defense and input prompt object 
 from defenses.smoothllm import SmoothLLM
 from defenses.smoothllm import Defense
 from defenses.smooth_prompt import smooth_prompt
 
 class SmoothLLMWrapper:
-    def __init__(
-        self,
-        base_model,
-        pert_type="RandomSwapPerturbation",
-        pert_pct=0.1,
-        num_copies=5,
-    ):
+    #contructor that takes in LocalSpeechLLM, perturb method, % of characters to perturb, & number of samples to take 
+    def __init__(self, base_model, pert_type="RandomSwapPerturbation", pert_pct=0.1, num_copies=5,):
         """
-        base_model: your LocalSpeechLLM
+        base_model: represents inputted LocalSpeechLLM
         """
         self.base_model = base_model
 
         # SmoothLLM expects target_model(batch=..., max_new_tokens=...)
-        # So we wrap base_model into callable format
+        # wrap base_model into callable format for smoothllm
         self.callable_model = self._build_callable_model()
 
         self.smoothllm = SmoothLLM(
@@ -28,8 +24,8 @@ class SmoothLLMWrapper:
 
     def _build_callable_model(self):
         """
-        Converts your LocalSpeechLLM into a callable model
-        that matches SmoothLLM expectations.
+        Converts LocalSpeechLLM into a callable model
+        that matches SmoothLLM input expectations.
         """
 
         base_model = self.base_model
@@ -38,8 +34,7 @@ class SmoothLLMWrapper:
             def __call__(self, batch, max_new_tokens):
                 outputs = []
                 for prompt_text in batch:
-                    # IMPORTANT: your base_model needs audio input
-                    # We assume audio path was stored in wrapper state
+                    # Using assumption that audio path is in base model
                     response = base_model.generate(
                         self.question_audio,
                         prompt_text,
@@ -54,9 +49,9 @@ class SmoothLLMWrapper:
         """
         This matches your existing attack interface.
         """
-        # Store audio so callable model can access it
+        #stores audio so callable model can access it
         self.callable_model.question_audio = question_audio
 
-        smooth_prompt = SmoothPrompt(prompt_text, max_tokens)
+        smooth_prompt = smooth_prompt(prompt_text, max_tokens)
 
         return self.smoothllm(smooth_prompt)
